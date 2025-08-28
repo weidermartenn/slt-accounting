cced9f82026dbc9fc260d522ccbc0c62683d13a8
wdrf14 <weidermartenn@gmail.com>
2025-08-28 23:19:13 +0300

    sixth commit

---
+++ stores/employee/store.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import type { Employee } from '~/entities/Employee/types' 
+
+export const useEmployeeStore = defineStore('employee', {
+    state: () => ({
+        employees: [] as Employee[],
+        loading: false,
+        error: '' as string | null,
+    }),
+
+    actions: {
+        async fetchEmployees() {
+            this.loading = true
+            this.error = ''
+            try {
+                const data = await $fetch<Employee[]>('/api/employee/namelist', {
+                    headers: process ? useRequestHeaders(['cookie']) : undefined
+                })
+                this.employees = data
+            } catch (e: any) {
+                this.error = e?.data?.statusMessage || e?.message || 'Ошибка загрузки'
+                throw e
+            } finally {
+                this.loading = false
+            }
+        }
+    }
+})
+
