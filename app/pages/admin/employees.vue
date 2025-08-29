d9504fa9324e24c8a02969cd2fee91d68d8e9a8b
weidermartenn <weidermartenn@gmail.com>
2025-08-29 17:55:57 +0300

    eighth commit

--- app/pages/admin/employees.vue
+++ app/pages/admin/employees.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script setup lang="ts">
+import { useEmployeeStore } from '~/stores/employee-store';
 
+const es = useEmployeeStore();
+const employees = computed(() => es.employees);
+
+onMounted(async () => {
+    await es.fetchEmployees();
+    console.log(employees.value);
+})
 </script>
 
 <style scoped>
